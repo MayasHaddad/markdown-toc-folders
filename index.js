b027237e01f5a655ae35c130544ec93230830cb7
Mayas Haddad <mayas_91@hotmail.com>
2021-01-22 17:39:36 +0100

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+
+const toc = require("markdown-toc");
+const walk = require("./walk");
+const path = require("path");
+const fs = require("fs/promises");
+const yargs = require("yargs");
+
+const args = yargs
+  .usage("Usage: $0 [options]")
+  .option("ignore", {
+    alias: "i",
+    type: "array",
+    description: "files and directories to ignore",
+  })
+  .option("output-file", {
+    alias: "o",
+    type: "string",
+    description: "path to the file to which output the Table of Content",
+  })
+  .help()
+  .alias("help", "h")
+  .example(
+    "$0 . -i foo.md dist/ -o README.md",
+    "builds Table of Content recursively for all files in current directory excluding foo.md and dist/* and writes the result to README.md"
+  ).argv;
+
+const run = async () => {
+  const { _, ignore: ignoredPaths, "output-file": outputFile } = args;
+  const res = await walk(_[0]);
+  const markdownFiles = res?.filter((name) => name.endsWith(".md"));
+  const files = markdownFiles?.map((e) => path.relative(process.cwd(), e));
+  const withoutIgnoredPaths = files?.filter(
+    (e) => !ignoredPaths?.find((ignoredPath) => e.startsWith(ignoredPath))
+  );
+
+  let totalContent = "";
+  for (let file of withoutIgnoredPaths) {
+    const currentFileContent = await fs.readFile(`./${file}`, "utf-8");
+    totalContent = `${totalContent}\n${currentFileContent}`;
+  }
+
+  const result = toc(totalContent).content;
+
+  if (!outputFile) {
+    console.log(result);
+    return;
+  }
+
+  fs.writeFile(outputFile, result, "utf8");
+};
+
+run();
